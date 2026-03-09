072d2e420f5f465248dd11afa4b7ded18a2f1642
56l39j09y-ship-it <56l39j09y@gmail.com>
2026-03-09 14:29:48 -0600

    Add GymsCommand class with subcommands for challengers

---
+++ src/main/kotlin/com/cobblegyms/commands/GymsCommand.kt
@@ -0,0 +1,32 @@
+package com.cobblegyms.commands
+
+class GymsCommand {
+
+    fun medallero() {
+        // Implementation to view medals and season progress
+    }
+
+    fun retarLideres() {
+        // Implementation to select and challenge gym leaders
+    }
+
+    fun retarE4() {
+        // Implementation to select and challenge elite four
+    }
+
+    fun retarCampeon() {
+        // Implementation to challenge champion
+    }
+
+    fun validacionDeEquipo() {
+        // Implementation to validate Pokemon team
+    }
+
+    fun reglas() {
+        // Implementation to view rules, bans, with search functionality
+    }
+
+    fun miCola() {
+        // Implementation to view queue status
+    }
+}
